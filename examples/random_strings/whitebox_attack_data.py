850b535ccd5f2283e922b5f6b0b6d64aa500968e
Brandon Edwards <brandon.edwards@intel.com>
2025-08-07 11:34:32 -0700

    more to do with script to create adversarial data, pushing recent changes

--- examples/random_strings/whitebox_attack_data.py
+++ examples/random_strings/whitebox_attack_data.py
@@ -18,18 +18,75 @@ from llmart import AdversarialTextGenerationPipeline
 from llmart import GreedyCoordinateGradient
 
 
+import os
+import sys
+
+import numpy as np
+import random
+import pickle as pkl
+
+from functools import partial
+
+from datasets import load_dataset
+import transformers
+
+
+
+
+pattern_to_replace_with_adv_tokens = f"<REPLACE HERE>"  # This is the pattern we will replace with the attack tokens
+
+# for the adversarial attack
+num_tokens = 10
+max_steps = 10
+
+adv_data_path = f"/raid/edwardsb/projects/llmart/data/adv_data_num_tokens_{num_tokens}_max_steps_{max_steps}.pkl"
+
+seed = 2025
+
+print(f"CUDA DEVICE environment variable set to: {os.environ['CUDA_VISIBLE_DEVICES']}")
+
+print(torch.__version__, torch.cuda.is_available())
+
+# Seed for reproducibility
+torch.manual_seed(seed)
+np.random.seed(seed)
+random.seed(seed)
+
+
+def form_queries(inputs):
+    """
+    Inputs is a list of dictionaries with keys 'instruction', and 'input', and this puts queries together with these that
+    tells the LLM how to see each part. The return is a list of strings.
+    """
+    return [f"Please perform the instruction provided following 'INSTRUCTION:' using the data provided after 'DATA:'.\n INSTRUCTION: {input['instruction']}, DATA:  {input['input']}" for input in inputs]
+
+
+
 def main(
-    sequence: str,
+    path_to_pickled_data_dicts: str,
+    attack_success_string: str, 
+    adv_data_pardir: str,
+    pattern_to_replace_with_adv_tokens: str,
     max_steps: int,
     num_tokens: int,
+    device: str,
     lr: float,
-    use_hard_tokens: bool,
+    use_hard_tokens: bool, 
+    seed: int,
 ):
+    os.environ["CUDA_VISIBLE_DEVICES"] = device.split(":")[1] if ":" in device else device
+
+    # unpickle the data dicts
+    with open(path_to_pickled_data_dicts, 'rb') as f:
+        data_dicts = pkl.load(f)
+
+    print(f"Using device: {device}")
+    print(f"\nIS CUDA AVAILABLE?: {torch.cuda.is_available()}\n")
     generator = pipeline(
         task="text-generation",
         model="meta-llama/Llama-2-7b-chat-hf",
         revision="f5db02db724555f92da89c216ac04704f23d4590",
-        device_map="auto",
+        device=device,
         do_sample=False,
         top_p=None,
         temperature=None,
@@ -40,11 +97,45 @@ def main(
     assert isinstance(generator.tokenizer, PreTrainedTokenizerBase)
     generator.tokenizer.pad_token = generator.tokenizer.eos_token
 
+    adv_data_fname = f"adv_data_num_tokens_{num_tokens}_max_steps_{max_steps}_seed_{seed}.pkl"
+    adv_data_path = os.path.join(adv_data_pardir, adv_data_fname)
+
+
+    adversarial_data = []
+    if not os.path.exists(adv_data_path):
+        print(f"Adversarial data file does not exist at {adv_data_path}, generating adversarial data...\n")
+        for data_dict_idx, data_dict in enumerate(data_dicts):
+            print(f"Processing data_dict {data_dict_idx + 1}/{len(data_dicts)}")
+            prompt = form_queries([data_dict])[0]
+
+            found, (adv_prompt, decoded) = find_prepend_tokens_to_data(attack_success_string,
+                                                                    device='cuda:0', 
+                                                                    pattern_to_replace_with_adv_tokens=pattern_to_replace_with_adv_tokens, 
+                                                                    prompt=prompt, 
+                                                                    generator=generator, 
+                                                                    num_tokens=num_tokens, 
+                                                                    max_steps=max_steps, 
+                                                                    lr=0.005, 
+                                                                    use_hard_tokens=True)
+            if found:
+                adversarial_data.append({'input': decoded + data_dict['input'], 'output': data_dict['output'], 'instruction': data_dict['instruction']})
+        print(f"Saving adversarial data to {adv_data_path} with {len(adversarial_data)} samples.")
+        with open(adv_data_path, 'wb') as f:
+            pkl.dump(adversarial_data, f)   
+    else:
+        raise ValueError(f"Adversarial data file already exists at {adv_data_path}. Please remove it or change the num_tokens/max_steps/seed parameters to generate new data.")
+
+
+
+
+
     print(
-        f"\nTrying to generate '{sequence}' of len={len(sequence)} | with: num_tokens({num_tokens})"
+        f"\nTrying to generate '{attack_success_string}' of len={len(attack_success_string)} | with: num_tokens({num_tokens})"
     )
-    found, (adv_prefix, gen_completion) = attack(
-        sequence=sequence,
+    found, (adv_prefix, gen_completion) = find_prepend_tokens_to_data(
+        attack_success_sring=attack_success_string,
+        device=device, 
+        pattern_to_replace_with_adv_tokens=pattern_to_replace_with_adv_tokens,
         generator=generator,
         num_tokens=num_tokens,
         max_steps=max_steps,
@@ -58,9 +149,8 @@ def main(
         print("Failed to find effective prompt")
 
 
-def attack(
-    sequence: str,
-    *,
+def find_prepend_tokens_to_data(
+    attack_success_string: str,
     device: str,
     pattern_to_replace_with_adv_tokens: str,
     prompt: str, 
@@ -71,7 +161,12 @@ def attack(
     use_hard_tokens: bool,
     seed: int = 2024,
 ) -> tuple[bool, tuple[str, str]]:
+    
+    # Seed for reproducibility
     torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
     adv_generator = pipeline(
         task="adv-text-generation",
         model=generator.model,
@@ -83,6 +178,7 @@ def attack(
         model_kwargs=dict(),
     )
     assert isinstance(adv_generator, AdversarialTextGenerationPipeline)
+
 
     if use_hard_tokens:
         optim = GreedyCoordinateGradient(
@@ -103,7 +199,7 @@ def attack(
 
     for _ in (pbar := trange(max_steps)):
         with torch.inference_mode():
-            found = adv_completion.startswith(sequence)
+            found = adv_completion.startswith(attack_success_string)
             if found:
                 break
 
@@ -150,15 +246,28 @@ def attack(
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "sequence",
+        "attack_success_string",
         type=str,
         help="Target generation/completion.",
+        default="\n\nAnswer: HACKED"
+    )
+    parser.add_argument(
+        "adv_data_pardir",
+        type=str,
+        help="Path to the directory holding the adversarial data file",
+        default="/raid/edwardsb/projects/llmart/data",
+    )
+    parser.add_argument(
+        "device",
+        type=str,
+        help="String indicating the device to use, e.g. 'cuda:0' or 'cpu'.",
+        default="cuda:1",
     )
     parser.add_argument(
         "--max_steps",
         dest="max_steps",
         type=int,
-        default=5000,
+        default=1000, # was 50,000 in the random_strings example
         help="Optimise prompt for no more than N steps.",
     )
     parser.add_argument(
